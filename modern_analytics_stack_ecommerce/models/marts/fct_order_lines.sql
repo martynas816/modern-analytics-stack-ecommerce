08d1fc82846c5a068c1d0fe6b64362c4f89cc604
Martynas Džiugas <martynas.dziugas2@gmail.com>
2026-02-24 14:02:16 +0200

    Clean up fct_order_lines.sql by removing comments
    Removed commented-out SQL select statement and extra blank lines.

--- modern_analytics_stack_ecommerce/models/marts/fct_order_lines.sql
+++ modern_analytics_stack_ecommerce/models/marts/fct_order_lines.sql
@@ -10,20 +10,3 @@ select
     line_revenue,
     is_cancelled
 from {{ ref('int_order_lines_enriched') }}
-
-
-
--- select
---     oi.invoice_id,
---     o.ordered_at::date as order_date,
---     oi.ordered_at,
---     oi.customer_id,
---     oi.country,
---     oi.product_id,
---     oi.quantity,
---     oi.unit_price,
---     oi.line_revenue,
---     o.is_cancelled
--- from {{ ref('stg_order_items') }} oi
--- left join {{ ref('stg_orders') }} o
---   on oi.invoice_id = o.invoice_id
